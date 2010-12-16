41c747821bcac0b9df8ca504d2b3528fb6a767b3
dennis <dennis.iversen@gmail.com>
2010-12-16 17:52:52 +0100

    removed 403 from login.

--- login/index.php
+++ login/index.php
@@ -3,11 +3,7 @@
 /**
  * @package    account
  */
-// check to see if user is allowed to use faccebook login
-if (!get_module_ini('use_email_login')){
-    moduleLoader::$status[403] = 1;
-    return;
-}
+
 
 // assign title to page
 template::setTitle(lang::translate('Login or logout'));
