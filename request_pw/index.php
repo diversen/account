d854a56b7d7105023657fc99c25720a0c166dd19
diversen <dennis.iversen@gmail.com>
2011-04-08 21:13:01 +0200

    updated account. lang. Use new form.

--- request_pw/index.php
+++ request_pw/index.php
@@ -7,17 +7,24 @@ template::setTitle(lang::translate('account_request_password_title'));
 
 $request = new request();
 if (isset($_POST['submit'])){
-    $mail_sent = $request->requestPassword($_POST['email']);
-    if (empty($request->errors)){        
+    $request->sanitize();
+    $request->validate();
+    
+    if (empty($request->errors)){
+        $mail_sent = $request->requestPassword($_POST['email']);
         if ($mail_sent){
-            view_confirm (lang::translate('account_request_login_info_sent_to') . ": " . $_POST['email']);
+            session::setActionMessage(
+                lang::translate('account_request_login_info_sent_to') .
+                MENU_SUB_SEPARATOR_SEC .
+                $_POST['email']
+            );
+            $location = get_module_ini('account_default_url');
+            $header = "Location: $location";
+            header($header);
         }
     } else {
         view_form_errors($request->errors);
-        view_request();
     }
 }
 
-if (!isset($mail_sent)){
-    view_request();
-}
+view_request();
--- request_pw/verify.php
+++ request_pw/verify.php
@@ -1,37 +1,26 @@
 <?php
 
-/**
- * @package    account
- */
 template::setTitle(lang::translate('account_request_set_new_passwords_title'));
 
 $request = new request();
 $res = $request->verifyAccount();
 if ($res){
     if (isset($_POST['submit'])){
-        $request->validate();
+        $request->sanitize();
         $request->validatePassword();
-        if (!empty($request->errors)){
-            view_form_errors($request->errors);
-            view_verify();
-        } else {
+        if (empty($request->errors)){
             if ($request->setNewPassword()){
                 session::setActionMessage(
                     lang::translate('account_request_new_password_has_been_set')
                 );
-                header("Location: /account/index");
+                $location = get_module_ini('account_default_url');
+                $header = "Location: $location";
+                header($header);
             }
+        } else {
+            view_form_errors($request->errors);
         }
-    } else {
-        view_verify();
-    }
-} else {
-    view_form_errors($request->errors);
+    } 
 }
 
-
-
-
-
-
-
+view_verify();
