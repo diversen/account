9d9b5b2a517065393eac0a66c7d4f8ecde141387
Dennis Iversen <dennis.iversen@gmail.com>
2011-12-10 10:27:42 +0100

    testing auto discover email with openid.

---
+++ login/create.php
@@ -0,0 +1,21 @@
+<?php
+
+include_module('account/create');
+if (!session::checkAccessControl('account_allow_create')){
+    return;
+}
+
+template::setTitle(lang::translate('account_create_index_title'));
+$account = new accountCreate();
+if (!empty($_POST['submit'])){
+    $account->validate();
+    if (empty($account->errors)){
+        $account->createUser();
+        view_confirm(lang::translate('account_create_account_has_been_created'));
+    } else {
+        view_form_errors($account->errors);
+        account_view_create();
+    }
+} else {
+    account_view_create();
+}
