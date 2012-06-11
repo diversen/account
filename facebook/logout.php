e0a5567f7ee222a57be9b805e900ecd6fc364e16
dennis <dennis@dennis-laptop.(none)>
2012-06-11 09:01:59 +0200

    minor changes

--- facebook/logout.php
+++ facebook/logout.php
@@ -6,7 +6,6 @@
 $facebook = facebook_get_object();
 $fb_key = 'fbs_'.config::getModuleIni('account_facebook_api_appid');
 setcookie($fb_key, '', 0, '', '/', '');
-//$facebook->setSession(NULL);
 session_destroy();
 
 header ("Location: /account/facebook/index");
