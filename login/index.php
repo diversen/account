aebbd24ba8b79dc2fd16ccc598110f2e51346a54
dennis <dennis@dennis-laptop.(none)>
2012-06-29 14:10:00 +0200

    keep session

--- login/index.php
+++ login/index.php
@@ -3,6 +3,12 @@
 http::prg();
 template::setTitle(lang::translate('account_login_or_logout'));
 
-$options = array ('auth_verified_only' => 1);
+$options = array();
+if (isset($_POST['keep_session']) && $_POST['keep_session'] == 1) {
+    $options['keep_session'] = 1;
+}
+
+$options['auth_verified_only'] = 1;
+    
 $login = new accountLogin($options);
 $login->controlLogin();
