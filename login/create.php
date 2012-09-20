3182ee4e3c037504b9b23558865dfd036ed7b7c2
dennis <dennis@dennis-laptop.(none)>
2012-09-20 10:03:46 +0200

    mv create from function to view

--- login/create.php
+++ login/create.php
@@ -8,14 +8,15 @@ if (!session::checkAccessControl('account_allow_create')){
 template::setTitle(lang::translate('account_create_index_title'));
 $l = new accountCreate();
 if (!empty($_POST['submit'])){
+    $_POST = html::specialEncode($_POST);
     $l->validate();
     if (empty($l->errors)){
         $l->createUser();
         view_confirm(lang::translate('account_create_account_has_been_created'));
     } else {
         view_form_errors($l->errors);
-        account_view_create();
+        echo view::get('account', 'create/create');
     }
 } else {
-    account_view_create();
+    echo view::get('account', 'create/create');
 }
