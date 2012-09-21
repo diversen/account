a9e40368bd323be3ec2a26ca2cdbf63c7e29042d
dennis <dennis@dennis-laptop.(none)>
2012-09-21 11:36:38 +0200

    use views for easy override for mails sent

--- requestpw/index.php
+++ requestpw/index.php
@@ -6,30 +6,3 @@
 
 accountRequestpw::disaplyRequestPassword();
 return;
-
-template::setTitle(lang::translate('account_request_password_title'));
-
-$request = new accountRequestpw();
-if (isset($_POST['submit'])){
-    $request->sanitize();
-    $request->validate();
-    
-    if (empty($request->errors)){
-        $mail_sent = $request->requestPassword($_POST['email']);
-        if ($mail_sent){
-            session::setActionMessage(
-                lang::translate('account_request_login_info_sent_to')
-            );
-            $location = config::getModuleIni('account_default_url');
-            //$header = "Location: $location";
-            http::locationHeader($location);
-        } else {
-            cos_debug("could not send mail:" . __FILE__);
-        }
-    } else {
-        view_form_errors($request->errors);
-    }
-}
-
-$request->displayRequest();
-//view_request();
