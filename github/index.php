6876d2b48366e7fd29d44629cffb8811c2f291af
dennis <dennis@dennis-Rev-1-0.(none)>
2013-04-11 22:47:06 +0200

    check if allowed to use github

--- github/index.php
+++ github/index.php
@@ -1,7 +1,16 @@
 <?php
 
 template::setTitle(lang::translate('account_login_or_logout'));
-    
+
+
+usleep(100000);
+
+// check to see if user is allowed to use faccebook login
+if (!in_array('github', config::getModuleIni('account_logins'))){
+    moduleloader::setStatus(403);
+    return;
+}
+
 $login = new accountGithub();
 $login->setAcceptUniqueOnlyEmail(true);
 $login->controlLogin();
