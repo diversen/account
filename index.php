6e3050273934105fd2e2eb442c541aef3351a68c
dennis <dennis@dennis-laptop>
2013-11-15 17:22:01 +0100

    add option for redirect after login

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 
 template::setTitle(lang::translate('Account'));
+
+if (isset($_GET['return_to'])) {
+    $_SESSION['redirect_on_login'] = rawurldecode($_GET['return_to']);
+}
+
 account_module::redirectDefault();
