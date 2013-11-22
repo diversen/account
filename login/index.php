aed0bd822704788aa24bc13c16358b18207bc8ff
dennis <dennis@dennis-laptop>
2013-11-22 17:02:02 +0100

    mv from file to action

--- login/index.php
+++ login/index.php
@@ -1,16 +1,3 @@
 <?php
 
-usleep(100000);
 
-http::prg();
-template::setTitle(lang::translate('Log in or Log out'));
-
-$options = array();
-if (isset($_POST['keep_session']) && $_POST['keep_session'] == 1) {
-    $options['keep_session'] = 1;
-}
-
-$options['auth_verified_only'] = 1;
-    
-$login = new account_login($options);
-$login->controlLogin();
--- login/module.php
+++ login/module.php
@@ -39,11 +39,29 @@ class account_login extends account {
         }
     }
     
+    public function indexAction () {
+        usleep(100000);
+
+        http::prg();
+        template::setTitle(lang::translate('Log in or Log out'));
+
+        $options = array();
+        if (isset($_POST['keep_session']) && $_POST['keep_session'] == 1) {
+            $options['keep_session'] = 1;
+        }
+
+        $options['auth_verified_only'] = 1;
+
+        $login = new account_login($options);
+        $login->controlLogin();
+    }
+    
 
    
 
     /**
      * method for controlling email login 
+     * this uses default controller found at 
      * 
      */
     public function controlLogin (){
