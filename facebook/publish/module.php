13ee3a0c3c6984c5d765ee5d672ba28426760269
dennis <dennis@dennis-Lenovo-G500.(none)>
2013-11-23 21:10:01 +0100

    test

--- facebook/publish/module.php
+++ facebook/publish/module.php
@@ -14,7 +14,7 @@ class account_facebook_publish extends account_facebook {
     public function testAction () {
         $facebook = $this->getFBObject();
         $user = $facebook->getUser();
-        //print_r($user);
+        print_r($user);
     }
     
     public function postAction () {
