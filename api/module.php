e657549c09d5078707f995db8fcb0e2e2f196130
Dennis Iversen <dennis.iversen@gmail.com>
2014-02-17 12:51:12 +0100

    more api

--- api/module.php
+++ api/module.php
@@ -2,18 +2,35 @@
 
 class account_api {
     
+    /**
+     * controler for /account/api/lock/[id]
+     * @return void
+     */
     public function lockAction () {
         if(session::isSuper()) {
             $id = uri::fragment(3);
-            $values = array ('locked' => 1);
-            $res = db_q::update('account')->values($values)->filter('id =', $id)->exec();
-            if ($res) {
+            if ($this->lock($id)) {
                 echo lang::translate('User with ID <span class="notranslate">{ID}</span> has been locked!', 
-                        array ('ID' => $id)
+                                      array ('ID' => $id)
                 );
             }
+            
         } else {
-            user::lockedSet403(lang::translate('Not sufficient privileges. Super user is required'));
+            error_module::$message = lang::translate('Not sufficient privileges. Super user is required');
+            moduleloader::setStatus(403);
+            return;
         }
     }
+    
+    /**
+     * lock an account 
+     * @param int $id
+     * @return boolean $res true on success else false
+     */
+    public function lock ($id) {
+        $values = array ('locked' => 1);
+        return db_q::update('account')->values($values)->filter('id =', $id)->exec();
+    }
 }
+
+class account_api_module extends account_api {}
