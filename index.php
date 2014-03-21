836a9b60bb8d16e5f92c6ed73dae57b25e165aa9
dennis <dennis@dennis-Inspiron-531.(none)>
2014-03-21 16:42:25 +0100

    up lang

--- index.php
+++ index.php
@@ -6,4 +6,8 @@ if (isset($_GET['return_to'])) {
     $_SESSION['return_to'] = rawurldecode($_GET['return_to']);
 }
 
+if (isset($_GET['message'])) {
+    session::setActionMessage(rawurldecode($_GET['message']));
+}
+
 account_module::redirectDefault();
