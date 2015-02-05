8fa994e807b9cd580c31c5627148e6ff9a80b624
Dennis Iversen <dennis.iversen@gmail.com>
2015-02-05 11:16:09 +0100

    up views

--- admin/views.php
+++ admin/views.php
@@ -101,7 +101,6 @@ class account_admin_views {
 
         echo "<div class=\"account_admin_user\">\n";
         echo user::getProfile($v, $date, array ('user_id'));
-
         
         echo lang::translate('ID') . MENU_SUB_SEPARATOR_SEC . $v['id'];
         echo "<br />";
