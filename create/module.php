31ec93ac33c649deed763dcf406e2e1cc4409632
Dennis Iversen <dennis.iversen@gmail.com>
2015-02-27 16:36:20 +0100

    lang

--- create/module.php
+++ create/module.php
@@ -57,7 +57,6 @@ class account_create extends account {
                 
                 $this->errors['email'] = lang::translate('Email already exists');
                 $account = $this->getUserFromEmail($_POST['email'], null);
-                print_r($account);
                 if ($account['type'] != 'email') {
                     $this->errors['type'] = lang::translate('Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>', array ('ACCOUNT_TYPE' => $account['type']));
                 }
