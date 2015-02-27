ec7c8870a3c268effb375854ef52813ba40cc149
Dennis Iversen <dennis.iversen@gmail.com>
2015-02-27 16:05:45 +0100

    auto commit

--- google/module.php
+++ google/module.php
@@ -142,6 +142,9 @@ class account_google extends account {
     public function auth($search) {
 
         $account = $this->googleAccountExist($search);
+        print_r($account);
+        print_r($this->errors);
+        die;
         if (!empty($this->errors)) {
             echo html::getErrors($this->errors);
             return;
