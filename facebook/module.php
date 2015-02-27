bfa61e088990296e9f465d17133894609503bf4b
Dennis Iversen <dennis.iversen@gmail.com>
2015-02-27 15:19:53 +0100

    refactor

--- facebook/module.php
+++ facebook/module.php
@@ -12,6 +12,22 @@ class account_facebook extends account {
     public function __construct($options = array()) {
         parent::__construct($options);
     }
+    
+    public function indexAction () {
+        /**
+ * controller for logging in with facebook
+ */
+usleep(100000);
+template::setTitle(lang::translate('Facebook Login'));
+$options = array ('keep_session' => 1);
+$fb = new account_facebook($options);
+$fb->setAcceptUniqueOnlyEmail();
+$fb->login();
+
+if (!empty($fb->errors)) {
+    html::errors($fb->errors);
+}
+    }
     /**
      * method for authorizing a user
      * @param   string  $facebook_url
