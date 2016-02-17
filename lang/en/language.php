a3d7aff81ca5fb1841c3ba32802878315d2a9638
Dennis Iversen <dennis.iversen@gmail.com>
2016-02-17 16:51:37 +0100

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,18 +2,33 @@
 
 $LANG = array();
 
-// Translation of file modules/account/module.php
+// Translation of file modules/account/create/module.php
 
-$LANG['You are logged in'] = 'You are logged in';
-$LANG['Account needs to be verified before you may log in'] = 'Account needs to be verified before you may log in';
-$LANG['Main account is of this type: '] = 'Main account is of this type: ';
-$LANG['This account has been locked'] = 'This account has been locked';
-// Translation of file modules/account/login/module.php
+$LANG['Verify Account'] = 'Verify Account';
+$LANG['Account is already verified'] = 'Account is already verified';
+$LANG['Account has been verified'] = 'Account has been verified';
+$LANG['Email already exists'] = 'Email already exists';
+$LANG['Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>'] = 'Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>';
+$LANG['That is not a valid email'] = 'That is not a valid email';
+$LANG['Incorrect answer to captcha test'] = 'Incorrect answer to captcha test';
+$LANG['Password needs to be 7 chars'] = 'Password needs to be 7 chars';
+$LANG['Passwords does not match'] = 'Passwords does not match';
+$LANG['Emails does not match'] = 'Emails does not match';
+$LANG['We could not send email. Try again later'] = 'We could not send email. Try again later';
+$LANG['Account created'] = 'Account created';
+$LANG['Wrong validation key on account creation'] = 'Wrong validation key on account creation';
+// Translation of file modules/account/facebook/views.php
+
+$LANG['Login using facebook'] = 'Login using facebook';
+$LANG['Logout'] = 'Logout';
+// Translation of file modules/account/facebook/module.php
+
+$LANG['Facebook Login'] = 'Facebook Login';
+$LANG['We will need your email. No login without email. Please try again!'] = 'We will need your email. No login without email. Please try again!';
+// Translation of file modules/account/github/module.php
 
 $LANG['Log in or Log out'] = 'Log in or Log out';
-$LANG['Create Account'] = 'Create Account';
-$LANG['Account has been created. Visit your email box and press the verification link.'] = 'Account has been created. Visit your email box and press the verification link.';
-$LANG['Not a correct email or password'] = 'Not a correct email or password';
+$LANG['Github login'] = 'Github login';
 // Translation of file modules/account/login/views.php
 
 $LANG['Create account'] = 'Create account';
@@ -29,36 +44,60 @@ $LANG['days'] = 'days';
 
 $LANG['Lost password'] = 'Lost password';
 $LANG['Create account'] = 'Create account';
-// Translation of file modules/account/views/mails/signup_message.inc
+// Translation of file modules/account/login/module.php
 
-$LANG['Hi'] = 'Hi';
-$LANG['You have connected with {SITE_NAME} and requested an account'] = 'You have connected with {SITE_NAME} and requested an account';
-$LANG['In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line'] = 'In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line';
-$LANG['Kind Regards'] = 'Kind Regards';
-// Translation of file modules/account/views/mails/request_password.inc
+$LANG['Log in or Log out'] = 'Log in or Log out';
+$LANG['Create Account'] = 'Create Account';
+$LANG['Account has been created. Visit your email box and press the verification link.'] = 'Account has been created. Visit your email box and press the verification link.';
+$LANG['Not a correct email or password'] = 'Not a correct email or password';
+// Translation of file modules/account/install.inc
 
-$LANG['Hi'] = 'Hi';
-$LANG['You have connected with {SITE_NAME} because you have lost your password'] = 'You have connected with {SITE_NAME} because you have lost your password';
-$LANG['In order to create a new password just press the following link or paste it into your browsers URL line'] = 'In order to create a new password just press the following link or paste it into your browsers URL line';
-$LANG['If you have not requested a new password then you can just delete this message'] = 'If you have not requested a new password then you can just delete this message';
-$LANG['Kind Regards'] = 'Kind Regards';
-// Translation of file modules/account/facebook/module.php
-
-$LANG['Facebook Login'] = 'Facebook Login';
-$LANG['We will need your email. No login without email. Please try again!'] = 'We will need your email. No login without email. Please try again!';
-// Translation of file modules/account/facebook/views.php
-
-$LANG['Login using facebook'] = 'Login using facebook';
+$LANG['account'] = 'account';
+$LANG['Account'] = 'Account';
+$LANG['Users'] = 'Users';
 $LANG['Logout'] = 'Logout';
-// Translation of file modules/account/admin/module.php
+// Translation of file modules/account/requestpw/views.php
 
-$LANG['Found the following accounts'] = 'Found the following accounts';
-$LANG['I could not find any matching results'] = 'I could not find any matching results';
-$LANG['Search for users'] = 'Search for users';
-$LANG['Edit account'] = 'Edit account';
-$LANG['Account has been updated'] = 'Account has been updated';
-$LANG['Password needs to be 7 chars'] = 'Password needs to be 7 chars';
+$LANG['Enter new password'] = 'Enter new password';
+$LANG['Password'] = 'Password';
+$LANG['Repeat new password'] = 'Repeat new password';
+$LANG['Send'] = 'Send';
+$LANG['Request new password'] = 'Request new password';
+$LANG['Email'] = 'Email';
+$LANG['Enter CAPTCHA string'] = 'Enter CAPTCHA string';
+// Translation of file modules/account/requestpw/module.php
+
+$LANG['Wrong answer to captcha test'] = 'Wrong answer to captcha test';
+$LANG['Email does not exists in our system'] = 'Email does not exists in our system';
+$LANG['Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>'] = 'Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>';
+$LANG['Create new password for site'] = 'Create new password for site';
+$LANG['Password has to be at least 7 chars'] = 'Password has to be at least 7 chars';
 $LANG['Passwords does not match'] = 'Passwords does not match';
+$LANG['No such combination between token and email. You can only use the token one time'] = 'No such combination between token and email. You can only use the token one time';
+$LANG['Create new password'] = 'Create new password';
+$LANG['New password has been saved'] = 'New password has been saved';
+$LANG['Request new password'] = 'Request new password';
+$LANG['Visit your mailbox and follow instructions in order to get a new password'] = 'Visit your mailbox and follow instructions in order to get a new password';
+$LANG['We could not not send you an email. Please try again later'] = 'We could not not send you an email. Please try again later';
+// Translation of file modules/account/views.php
+
+$LANG['By registering, you agree to the <a href="/account/terms/privacy">privacy policy</a> and <a href="/account/terms/service">terms of service</a>.'] = 'By registering, you agree to the <a href="/account/terms/privacy">privacy policy</a> and <a href="/account/terms/service">terms of service</a>.';
+// Translation of file modules/account/google/module.php
+
+$LANG['No google access token'] = 'No google access token';
+$LANG['Your google email needs to be verified'] = 'Your google email needs to be verified';
+$LANG['Could not get google client access token. Try again later.'] = 'Could not get google client access token. Try again later.';
+$LANG['Log in or Log out'] = 'Log in or Log out';
+$LANG['Google login'] = 'Google login';
+$LANG['We could not merge accounts. Try again later.'] = 'We could not merge accounts. Try again later.';
+// Translation of file modules/account/menu.inc
+
+$LANG['Email'] = 'Email';
+$LANG['OpenID'] = 'OpenID';
+$LANG['Facebook'] = 'Facebook';
+$LANG['Github'] = 'Github';
+$LANG['Google'] = 'Google';
+$LANG['Admin'] = 'Admin';
 // Translation of file modules/account/admin/views.php
 
 $LANG['Edit account'] = 'Edit account';
@@ -81,70 +120,31 @@ $LANG['Delete'] = 'Delete';
 $LANG['Search accounts'] = 'Search accounts';
 $LANG['Search account ID'] = 'Search account ID';
 $LANG['Search'] = 'Search';
-// Translation of file modules/account/github/module.php
+// Translation of file modules/account/admin/module.php
 
-$LANG['Log in or Log out'] = 'Log in or Log out';
-$LANG['Github login'] = 'Github login';
-// Translation of file modules/account/install.inc
-
-$LANG['account'] = 'account';
-$LANG['Account'] = 'Account';
-$LANG['Users'] = 'Users';
-$LANG['Logout'] = 'Logout';
-// Translation of file modules/account/views.php
-
-$LANG['By registering, you agree to the <a href="/account/terms/privacy">privacy policy</a> and <a href="/account/terms/service">terms of service</a>.'] = 'By registering, you agree to the <a href="/account/terms/privacy">privacy policy</a> and <a href="/account/terms/service">terms of service</a>.';
-// Translation of file modules/account/requestpw/module.php
-
-$LANG['Wrong answer to captcha test'] = 'Wrong answer to captcha test';
-$LANG['Email does not exists in our system'] = 'Email does not exists in our system';
-$LANG['Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>'] = 'Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>';
-$LANG['Create new password for site'] = 'Create new password for site';
-$LANG['Password has to be at least 7 chars'] = 'Password has to be at least 7 chars';
-$LANG['Passwords does not match'] = 'Passwords does not match';
-$LANG['No such combination between token and email. You can only use the token one time'] = 'No such combination between token and email. You can only use the token one time';
-$LANG['Create new password'] = 'Create new password';
-$LANG['New password has been saved'] = 'New password has been saved';
-$LANG['Request new password'] = 'Request new password';
-$LANG['Visit your mailbox and follow instructions in order to get a new password'] = 'Visit your mailbox and follow instructions in order to get a new password';
-$LANG['We could not not send you an email. Please try again later'] = 'We could not not send you an email. Please try again later';
-// Translation of file modules/account/requestpw/views.php
-
-$LANG['Enter new password'] = 'Enter new password';
-$LANG['Password'] = 'Password';
-$LANG['Repeat new password'] = 'Repeat new password';
-$LANG['Send'] = 'Send';
-$LANG['Request new password'] = 'Request new password';
-$LANG['Email'] = 'Email';
-$LANG['Enter CAPTCHA string'] = 'Enter CAPTCHA string';
-// Translation of file modules/account/menu.inc
-
-$LANG['Email'] = 'Email';
-$LANG['OpenID'] = 'OpenID';
-$LANG['Facebook'] = 'Facebook';
-$LANG['Github'] = 'Github';
-$LANG['Google'] = 'Google';
-$LANG['Admin'] = 'Admin';
-// Translation of file modules/account/google/module.php
-
-$LANG['No google access token'] = 'No google access token';
-$LANG['Your google email needs to be verified'] = 'Your google email needs to be verified';
-$LANG['Could not get google client access token. Try again later.'] = 'Could not get google client access token. Try again later.';
-$LANG['Log in or Log out'] = 'Log in or Log out';
-$LANG['Google login'] = 'Google login';
-$LANG['We could not merge accounts. Try again later.'] = 'We could not merge accounts. Try again later.';
-// Translation of file modules/account/create/module.php
-
-$LANG['Verify Account'] = 'Verify Account';
-$LANG['Account is already verified'] = 'Account is already verified';
-$LANG['Account has been verified'] = 'Account has been verified';
-$LANG['Email already exists'] = 'Email already exists';
-$LANG['Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>'] = 'Email is connected to an account of this type: <span class="notranslate">{ACCOUNT_TYPE}</span>';
-$LANG['That is not a valid email'] = 'That is not a valid email';
-$LANG['Incorrect answer to captcha test'] = 'Incorrect answer to captcha test';
+$LANG['Found the following accounts'] = 'Found the following accounts';
+$LANG['I could not find any matching results'] = 'I could not find any matching results';
+$LANG['Search for users'] = 'Search for users';
+$LANG['Edit account'] = 'Edit account';
+$LANG['Account has been updated'] = 'Account has been updated';
 $LANG['Password needs to be 7 chars'] = 'Password needs to be 7 chars';
 $LANG['Passwords does not match'] = 'Passwords does not match';
-$LANG['Emails does not match'] = 'Emails does not match';
-$LANG['We could not send email. Try again later'] = 'We could not send email. Try again later';
-$LANG['Account created'] = 'Account created';
-$LANG['Wrong validation key on account creation'] = 'Wrong validation key on account creation';
+// Translation of file modules/account/module.php
+
+$LANG['You are logged in'] = 'You are logged in';
+$LANG['Account needs to be verified before you may log in'] = 'Account needs to be verified before you may log in';
+$LANG['Main account is of this type: '] = 'Main account is of this type: ';
+$LANG['This account has been locked'] = 'This account has been locked';
+// Translation of file modules/account/views/mails/signup_message.inc
+
+$LANG['Hi'] = 'Hi';
+$LANG['You have connected with {SITE_NAME} and requested an account'] = 'You have connected with {SITE_NAME} and requested an account';
+$LANG['In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line'] = 'In order to activate your account, you just have to press the following link or copy and paste into your browsers URL line';
+$LANG['Kind Regards'] = 'Kind Regards';
+// Translation of file modules/account/views/mails/request_password.inc
+
+$LANG['Hi'] = 'Hi';
+$LANG['You have connected with {SITE_NAME} because you have lost your password'] = 'You have connected with {SITE_NAME} because you have lost your password';
+$LANG['In order to create a new password just press the following link or paste it into your browsers URL line'] = 'In order to create a new password just press the following link or paste it into your browsers URL line';
+$LANG['If you have not requested a new password then you can just delete this message'] = 'If you have not requested a new password then you can just delete this message';
+$LANG['Kind Regards'] = 'Kind Regards';
